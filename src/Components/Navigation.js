ceb0ecf163bff4769011ac7782f135b0f5070ae3
Atthaset <iaut2545@gmail.com>
2023-02-09 16:16:52 +0700

    9/2/2023-2

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,16 +1,41 @@
 import { Link } from "react-router-dom"
-import { FaBars } from "react-icons/fa";
+import { FaBars, FaHome } from "react-icons/fa";
+import { useState } from "react";
+import './Navigation.css'
 
 const Navigation = () => {
+
+    const [showMenu, setShowMenu] = useState(false)
+    const toggleMenu = () => setShowMenu(!showMenu)
+
     return (
         <aside>
             <div className="navbar">
                 <div className="navbar-toggle">
                     <Link to="#" className="menubar">
-                        <FaBars />
+                        <FaBars onClick={toggleMenu} />
                     </Link>
                 </div>
             </div>
+            <nav className={showMenu ? "nav-menu active" : "nav-menu"}>
+                <ul className="nav-menu-item">
+                    <li className="menu-text">
+                        <Link to="#">
+                            <FaHome /><span>หน้าแรก</span>
+                        </Link>
+                    </li>
+                    <li className="menu-text">
+                        <Link to="#">
+                            <FaHome /><span>หน้าแรก</span>
+                        </Link>
+                    </li>
+                    <li className="menu-text">
+                        <Link to="#">
+                            <FaHome /><span>หน้าแรก</span>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
         </aside>
     )
 }
